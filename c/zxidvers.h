de58d0ae79b614a93b642019b9c33625f79c653d
Sampo Kellomaki <sampo@ami.(none)>
2011-03-02 20:05:19 +0000

    .

--- c/zxidvers.h
+++ c/zxidvers.h
@@ -1,7 +1,7 @@
 #ifndef _zxidvers_h
 #define _zxidvers_h
-#define ZXID_VERSION 0x000078
-#define ZXID_REL "0.78"
-#define ZXID_COMPILE_DATE "1298938433"
-#define ZXID_REV "$Id: 0.77-12-gc07cbe3 20110223-042056 sampo@ $"
+#define ZXID_VERSION 0x000080
+#define ZXID_REL "0.80"
+#define ZXID_COMPILE_DATE "1299094710"
+#define ZXID_REV "$Id: 0.79 20110302-192424 sampo@ $"
 #endif
--- zxidjava/zxid_wrap.c
+++ zxidjava/zxid_wrap.c
@@ -23371,7 +23371,7 @@ SWIGEXPORT jint JNICALL Java_zxidjava_zxidjniJNI_ZXID_1VERSION_1get(JNIEnv *jenv
   
   (void)jenv;
   (void)jcls;
-  result = (int) 0x000078;
+  result = (int) 0x000080;
   jresult = (jint)result; 
   return jresult;
 }
@@ -23383,7 +23383,7 @@ SWIGEXPORT jstring JNICALL Java_zxidjava_zxidjniJNI_ZXID_1REL_1get(JNIEnv *jenv,
   
   (void)jenv;
   (void)jcls;
-  result = (char *) "0.78";
+  result = (char *) "0.80";
   if (result) jresult = (*jenv)->NewStringUTF(jenv, ( char *)result);
   return jresult;
 }
@@ -23395,7 +23395,7 @@ SWIGEXPORT jstring JNICALL Java_zxidjava_zxidjniJNI_ZXID_1COMPILE_1DATE_1get(JNI
   
   (void)jenv;
   (void)jcls;
-  result = (char *) "1298938433";
+  result = (char *) "1299094710";
   if (result) jresult = (*jenv)->NewStringUTF(jenv, ( char *)result);
   return jresult;
 }
@@ -23407,7 +23407,7 @@ SWIGEXPORT jstring JNICALL Java_zxidjava_zxidjniJNI_ZXID_1REV_1get(JNIEnv *jenv,
   
   (void)jenv;
   (void)jcls;
-  result = (char *) "$Id: 0.77-12-gc07cbe3 20110223-042056 sampo@ $";
+  result = (char *) "$Id: 0.79 20110302-192424 sampo@ $";
   if (result) jresult = (*jenv)->NewStringUTF(jenv, ( char *)result);
   return jresult;
 }
