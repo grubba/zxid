c711b02aec5c8923c1f9c5303e2caf1a23d9cb89
Sampo Kellomaki <sampo@saz.(none)>
2012-09-28 06:37:57 -0100

    .

---
+++ c/zxidvers.h
@@ -0,0 +1,7 @@
+#ifndef _zxidvers_h
+#define _zxidvers_h
+#define ZXID_VERSION 0x000111
+#define ZXID_REL "1.11"
+#define ZXID_COMPILE_DATE "1345312910"
+#define ZXID_REV "$Id:  20120818-184418 sampo@ $"
+#endif
