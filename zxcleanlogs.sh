9341dc820ee6241813e71b1c3d3dd3084d166d23
Sampo Kellomaki <sampo@zxid.org>
2013-04-05 20:01:39 +0000

    .

--- zxcleanlogs.sh
+++ zxcleanlogs.sh
@@ -5,6 +5,16 @@
 # Usage: ./zxcleanlogs.sh     # By default cleans /var/zxid/
 #        ./zxcleanlogs.sh /var/zxid/idp
 
+warning="WARNING: Running this script (zxcleanlogs.sh) in production environment
+         will destroy valuable audit trail data, which you may be legally obliged
+         to retain. Be sure any such data has already been copied to a safe place.
+
+         You should use zxlogclean.sh instead as it provides sane options to
+         preserve the audit trail.
+"
+
+echo $warning
+
 ZXID_PATH=$1
 if [ "x$ZXID_PATH" = "x" ] ; then ZXID_PATH=/var/zxid/; fi
 
