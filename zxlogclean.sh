49c1c6581e43a1d34df8f4a688c9b651c6700c9d
Sampo Kellomaki <sampo@zxid.org>
2013-04-05 21:20:22 +0000

    .

--- zxlogclean.sh
+++ zxlogclean.sh
@@ -4,7 +4,7 @@
 FIND=find
 RM="rm -rf"
 PGP=gpg
-#zalgo=--compress-algo bzip2
+#zalgo=--compress-algo bzip2     # seems bzip2 does not help much for audit data
 ses_atime=1
 trail_atime=30
 backupdir=~/zxbackup
@@ -56,6 +56,9 @@ Options:
 
 For -pgp and -sig options to work, you must have created keyrings, see $PGP --gen-key
 
+To decrypt (and verify signature, if any):
+gpg --secret-keyring log-decrypt-secret.gpg <~/zxbackup/trail-20130405-194808.tar.pgp | tar xvf -
+
 BUGS: The -pgp options may be still buggy (20130404 --Sampo)
 "
 
@@ -63,6 +66,7 @@ warn() { echo "$1" 1>&2; }
 die()  { echo "$1" 1>&2; exit 1; }
 
 while [ 1 ]; do
+#warn "HERE($1)"
 case "$1" in
 -d)  debug=1;  shift; continue;;
 -n)  dryrun=1; shift; continue;;
@@ -71,9 +75,9 @@ case "$1" in
 -f)  force=1;  shift; warn "Force not supported, see -r and -b\n$help"; continue;;
 -pgp)    shift;  pubkey=$1;   shift; continue;;
 -sig)    shift;  sig="-u $1 -s";   shift; continue;;
--sym)    shift;  symkeyf=$1;  shift; passphrase="--passphrase-file $symkeyf"; continue;;
--symfd)  shift;  symkeyfd=$1; shift; passphrase="--passphrase-fd $symkeyfd"; continue;;
--symstr) shift;  symkeys=$1;  shift; passphrase="--passphrase $symkeys"; continue;;
+-sym)    shift;  symkeyf=$1;  shift; symenc=1; passphrase="--passphrase-file $symkeyf"; continue;;
+-symfd)  shift;  symkeyfd=$1; shift; symenc=1; passphrase="--passphrase-fd $symkeyfd"; continue;;
+-symstr) shift;  symkeys=$1;  shift; symenc=1; passphrase="--passphrase $symkeys"; continue;;
 -pubring) shift; pubring="--no-default-keyring --keyring $1";   shift; continue;;
 -secring) shift; secring="--secret-keyring $1";   shift; continue;;
 -bdir)   shift;  backupdir=$1;   shift; continue;;
@@ -84,7 +88,7 @@ case "$1" in
 -h)  echo "$help"; exit;;
 --)  shift; break;;
 -*)  die "Unknown option: $1\n$help";;
-"")  break;;
+*)   break;;
 esac
 done
 
@@ -103,7 +107,7 @@ tarup() {
 
 pgppub() {
     #[ ! -r $pubkey ] && die "Public key($pubkey) not found or not readable ($?)"
-    if cat $2 | xargs tar cf - | $PGP $zalgo $pubring $secring -r $pubkey -e $sig --batch > $1 ; then
+    if cat $2 | xargs tar cf - | $PGP $zalgo $pubring $secring $sig -r $pubkey -e --batch > $1 ; then
 	[ $debug ] && warn "pgppub: Created backup($1)"
 	[ $nodel ] || xargs $RM < $2
     else
@@ -113,7 +117,8 @@ pgppub() {
 
 pgpsym() {
     #[ ! -r $symkey ] && die "Symmetric key($pubkey) not found or not readable ($?)"
-    if xargs tar cf - < $2 | $PGP -$zalgo $pubring $secring $passphrase -c $sig --batch > $1 ; then
+    #warn "$PGP $zalgo $pubring $secring $sig --force-mdc $passphrase -c --batch"
+    if xargs tar cf - < $2 | $PGP $zalgo $pubring $secring $sig --force-mdc $passphrase -c --batch > $1 ; then
 	[ $debug ] && warn "pgpsym: Created backup($1)"
 	[ $nodel ] || xargs $RM < $2
     else
@@ -126,16 +131,20 @@ pgpsym() {
 clean_inst() {
     dir=$1
 
+    #mkdir $dir/log/issue $dir/log/rely     # in case we accidentally delete them
+    #chown si $dir/log/issue $dir/log/rely
+    #chmod g+s $dir/log/issue $dir/log/rely
+
     [ $debug ] && warn "cleaning $dir"
 
     # Immediate cleanup items
 
-    echo log/xml.dbg > $backupdir/zxlogclean-todel-$$
+    echo "$dir/log/xml.dbg" > $backupdir/zxlogclean-todel-$$
 
     if [ $dryrun ] ; then cat $backupdir/zxlogclean-todel-$$
     elif [ "$backup" -a "$pubkey" ] ; then
 	pgppub $backupdir/junk-$now.tar.pgp $backupdir/zxlogclean-todel-$$
-    elif [ "$backup" -a "$symkey" ] ; then
+    elif [ "$backup" -a "$symenc" ] ; then
 	pgpsym $backupdir/junk-$now-sym.tar.pgp $backupdir/zxlogclean-todel-$$
     elif [ $backup ] ; then
 	tarup $backupdir/junk-$now.tgz $backupdir/zxlogclean-todel-$$
@@ -147,7 +156,9 @@ clean_inst() {
 
     # Audit trail
 
-    $FIND $dir/log \! -name .keep -atime +$trail_atime >$backupdir/zxlogclean-trail-$$
+    $FIND $dir/log -mindepth 2 \! -name .keep -atime +$trail_atime >$backupdir/zxlogclean-trail-$$
+    echo "$dir/log/err" >> $backupdir/zxlogclean-trail-$$
+    echo "$dir/log/act" >> $backupdir/zxlogclean-trail-$$
 
     #echo "Reset $now" > $dir/log/err
     #echo log/err* > $backupdir/zxlogclean-trail-$$
@@ -156,7 +167,7 @@ clean_inst() {
     if [ $dryrun ] ; then cat $backupdir/zxlogclean-trail-$$
     elif [ $pubkey ] ; then
 	pgppub $backupdir/trail-$now.tar.pgp $backupdir/zxlogclean-trail-$$
-    elif [ $symkey ] ; then
+    elif [ $symenc ] ; then
 	pgpsym $backupdir/trail-$now-sym.tar.pgp $backupdir/zxlogclean-trail-$$
     elif [ "$remove" -a !"$backup" ] ; then
 	[ $nodel ] || xargs $RM <$backupdir/zxlogclean-trail-$$
@@ -168,12 +179,12 @@ clean_inst() {
 
     # Session
 
-    $FIND $dir/ses -depth -atime +$ses_atime -type d >$backupdir/zxlogclean-ses-to-rm-$$
+    $FIND $dir/ses -mindepth 1 -depth -atime +$ses_atime -type d >$backupdir/zxlogclean-ses-to-rm-$$
     
     if [ $dryrun ] ; then cat $backupdir/zxlogclean-ses-to-rm-$$
     elif [ "$backup" -a "$pubkey" ] ; then
 	pgppub $backupdir/ses-$now.tar.pgp $backupdir/zxlogclean-ses-to-rm-$$
-    elif [ "$backup" -a "$symkey" ] ; then
+    elif [ "$backup" -a "$symenc" ] ; then
 	pgpsym $backupdir/ses-$now-sym.tar.pgp $backupdir/zxlogclean-ses-to-rm-$$
     elif [ $backup ] ; then
 	tarup $backupdir/ses-$now.tgz $backupdir/zxlogclean-ses-to-rm-$$
@@ -190,8 +201,9 @@ clean_inst() {
 
 if [ "x$*" = "x" ] ; then clean_inst `pwd`; exit; fi
 
-for inst in $* do
-  if [ !-d $instance ] ; then warn "Instance($inst) is not a directory. Skipping."; continue; fi
+for inst in $* ; do
+  if [ ! -d $inst ] ; then warn "Instance($inst) is not a directory. Skipping."; continue; fi
+  if [ ! -d $inst/log ] ; then warn "Inst($inst) is missing log. Skipping."; continue; fi
   clean_inst $inst
 done
 
