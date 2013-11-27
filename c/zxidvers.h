8ed0f2070b955e57d03e960ccf42fe390b344c21
Sampo Kellomaki <sampo@zxid.org>
2013-11-27 20:40:11 +0000

    .

--- c/zxidvers.h
+++ c/zxidvers.h
@@ -1,7 +1,7 @@
 #ifndef _zxidvers_h
 #define _zxidvers_h
-#define ZXID_VERSION 0x000118
-#define ZXID_REL "1.18"
+#define ZXID_VERSION 0x000119
+#define ZXID_REL "1.19"
 #define ZXID_COMPILE_DATE "1385227874"
-#define ZXID_REV "$Id: 1.18-7-g8e5679c 20131122-155334 sampo@ $"
+#define ZXID_REV "$Id: 1.19-7-g8e5679c 20131126-155334 sampo@ $"
 #endif
