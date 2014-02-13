07bc1f884dae1cb8ce00aa31d9bcb90a9ef8b899
Sampo Kellomaki <sampo@zxid.org>
2014-02-13 18:40:08 +0000

    zxidhlo debug out.

--- zxidhlo.c
+++ zxidhlo.c
@@ -85,7 +85,7 @@ int main(int argc, char** argv)
       perror("/var/tmp/zxid.stderr");
       exit(2);
     }
-  }
+  //}
   fprintf(stderr, "=================== Running " ZXIDHLO " ===================\n");
 #endif
 
