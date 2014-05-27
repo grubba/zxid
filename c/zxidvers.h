b5ca97474774b221948d99c970fbb465782a9873
Sampo Kellomaki <sampo@zxid.org>
2014-05-27 19:20:10 -0100

    Regenerated dependent files.

--- c/zxidvers.h
+++ c/zxidvers.h
@@ -2,6 +2,6 @@
 #define _zxidvers_h
 #define ZXID_VERSION 0x000121
 #define ZXID_REL "1.21"
-#define ZXID_COMPILE_DATE "1394678465"
-#define ZXID_REV "$Id: 1.21-2-g377f0a1 20140313-022443 sampo@ $"
+#define ZXID_COMPILE_DATE "1401221808"
+#define ZXID_REV "$Id: 1.21-13-g784059a 20140527-215832 sampo@ $"
 #endif
