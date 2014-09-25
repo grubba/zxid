9e02466e01cb58d7ebaa9ae500d026f2cd5d98b5
Sampo Kellomaki <sampo@zxid.org>
2014-09-25 19:20:11 +0200

    Version number fixup.

--- c/zxidvers.h
+++ c/zxidvers.h
@@ -2,6 +2,6 @@
 #define _zxidvers_h
 #define ZXID_VERSION 0x000122
 #define ZXID_REL "1.22"
-#define ZXID_COMPILE_DATE "1411645366"
-#define ZXID_REV "$Id: 1.21-15-gb5ca974 20140527-214243 sampo@ $"
+#define ZXID_COMPILE_DATE "1411665594"
+#define ZXID_REV "$Id: 1.21-17-gf42ac0e 20140925-175921 sampo@ $"
 #endif
