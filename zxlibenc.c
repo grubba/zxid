d94940cbeb6dc19a57e847c63f223a694d51d0e9
Henrik Grubbström (Grubba) <grubba@grubba.org>
2014-11-05 16:55:39 +0100

    libenc: Kludge the length calculation in zx_EASY_ENC_elem().
    The length calculation in zx_LEN_WO_any_elem() is still off,
    so just make sure that the buffer is big enough for now.

--- zxlibenc.c
+++ zxlibenc.c
@@ -330,7 +330,7 @@ struct zx_str* zx_EASY_ENC_elem(struct zx_ctx* c, struct zx_elem_s* x)
     return 0;
   }
   len = zx_LEN_WO_any_elem(c, x);
-  buf = ZX_ALLOC(c, len+1);
+  buf = ZX_ALLOC(c, len*2+1);
   p = zx_ENC_WO_any_elem(c, x, buf);
   if (p != buf+len) {
     ERR("Encoded length(%d) does not match computed length(%d). ED(%.*s)", ((int)(p-buf)), len, ((int)(p-buf)), buf);
